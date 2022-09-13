25587a238bada7c8762368535c9eba21856cf422
Srujan <srujan@Srujans-MacBook-Air.local>
2022-09-13 17:03:57 +0530

    updated pages and routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import {BrowserRouter as Router,Route} from 'react-router-dom'
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 import Navbar from './components/layouts/Navbar'
 import Footer from './components/layouts/Footer'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Notfound from './Pages/Notfound'
+
 
 
 function App() {
@@ -9,7 +13,15 @@ function App() {
     <div className="flex flex-col justify-between h-screen">
     <Navbar />
     <main className="container mx-auto px-3 pb-12">
-      srujan
+    <Routes>
+    <Route path='/' element={<Home />}/>
+    <Route path='/About' element={<About />}/>
+    <Route path='/Notfound' element={<Notfound />}/>
+    <Route path='/*' element={<Notfound />}/>
+
+
+    </Routes>
+      
     </main>
 
     <Footer />
