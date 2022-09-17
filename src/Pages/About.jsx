3bb8831f7f5b185ad8a07e61b67c072623c101c6
Srujan <srujan@Srujans-MacBook-Air.local>
2022-09-17 23:15:12 +0530

    updated about page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 
 function About() {
-  return (
-    <div className="container">
-      <div className="flex flex-row justify-between">
-       <div className="flex">hai</div>
-       <div className="flex">Bye</div>
-      </div>
-     
-    </div>
-  )
+  return <>
+  <h1 className='text-6xl mb-4'>Github Finder</h1>
+  <p className='mb-4 text-2xl font-light'>
+    A React app to search GitHub profiles and see profile details. 
+    .
+  </p>
+  <p className='text-lg text-gray-400'>
+    Version <span className='text-white'>1.0.0</span>
+  </p>
+  <p className='text-lg text-gray-400'>
+    Layout By:
+    <a className='text-white' href='https://twitter.com/This_IsSrujan'>
+     Srujan Putta
+    </a>
+  </p>
+  </>
 }
 
 export default About
