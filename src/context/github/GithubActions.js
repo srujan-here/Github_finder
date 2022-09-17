3960a440410a00266a32294bd8713b2095a32db0
Srujan <srujan@Srujans-MacBook-Air.local>
2022-09-17 23:09:38 +0530

    updated everything

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,5 +1,12 @@
+import axios from 'axios'
+
+
 const GITHUB_URL=process.env.REACT_APP_GITHUB_URL
 const GITHUB_TOKEN=process.env.REACT_APP_GITHUB_TOKEN
+
+const github=axios.create({
+    baseURL: GITHUB_URL
+})
 
 export const searchusers = async (text)=>{
    
